ebc53b7a2995494d28503c3113aaf583d540f9b0
wyatth7 <whardin1010@gmail.com>
2020-10-06 13:32:38 -0400

    Updates

---
+++ precache-manifest.dfdfb980b6f5b77d396807efced50c49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b43911dfd0cb2758bc2f389db5f04a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "959864c3d7c5c1f263c4",
+    "url": "/static/css/main.b3a1964e.chunk.css"
+  },
+  {
+    "revision": "f59144ce5e1c7adc0e52",
+    "url": "/static/js/2.10b87e74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.10b87e74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "959864c3d7c5c1f263c4",
+    "url": "/static/js/main.6cc61657.chunk.js"
+  },
+  {
+    "revision": "8bc9d5837075633a8b93",
+    "url": "/static/js/runtime-main.c96b335c.js"
+  }
+]);
